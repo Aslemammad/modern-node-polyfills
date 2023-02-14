46467c1ed62b718609d3742571c7e4b2d06a1580
Christian Bromann <git@bromann.dev>
2023-02-14 12:45:37 -0600

    fix tests in windows

--- basic.test.ts
+++ basic.test.ts
@@ -1,5 +1,6 @@
+import path from 'node:path'
 import { build } from "esbuild";
-import { readFile, writeFile, unlink } from "fs/promises";
+import { writeFile, unlink } from "fs/promises";
 import { expect, test } from "vitest";
 
 import {
@@ -10,14 +11,18 @@ import {
 } from "./index";
 
 test("polyfillPath", async () => {
-  expect((await polyfillPath("fs")).endsWith('@jspm/core/nodelibs/browser/fs.js'))
-    .toBe(true);
-  expect((await polyfillPath("node:fs")).endsWith('@jspm/core/nodelibs/browser/fs.js'))
-    .toBe(true);
-  expect((await polyfillPath("node:fs/promises")).endsWith('@jspm/core/nodelibs/browser/fs/promises.js'))
-    .toBe(true);
-  expect((await polyfillPath("http")).endsWith('@jspm/core/nodelibs/browser/http.js'))
-    .toBe(true);
+  expect((await polyfillPath("fs"))
+    .endsWith(path.join('@jspm', 'core', 'nodelibs', 'browser', 'fs.js'))
+  ).toBe(true);
+  expect((await polyfillPath("node:fs"))
+    .endsWith(path.join('@jspm', 'core', 'nodelibs', 'browser', 'fs.js'))
+  ).toBe(true);
+  expect((await polyfillPath("node:fs', 'promises"))
+    .endsWith(path.join('@jspm', 'core', 'nodelibs', 'browser', 'fs', 'promises.js'))
+  ).toBe(true);
+  expect((await polyfillPath("http"))
+    .endsWith(path.join('@jspm', 'core', 'nodelibs', 'browser', 'http.js'))
+  ).toBe(true);
   expect(polyfillPath("wrong")).rejects.toThrowError();
 });
 
